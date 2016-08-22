66f7badc4a57d64806aee5801dbc9815ede4cc25
Neiyo <403130374@qq.com>
2016-08-22 17:27:46 +0800

    Javascript Design

--- design/AOPDesign.js
+++ design/AOPDesign.js
@@ -1,1 +1,15 @@
-Function
+Function.prototype.before = function(beforefn){
+  var _self = this;
+  return function(){
+    before.apply(this,arguments);
+    return _self.apply(this,arguments);
+  }
+}
+
+var func = function(){
+  console.log(2);
+}
+func =  func.before(function(){
+   console.log(1);
+});
+func();
